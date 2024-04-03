97167e0c1bd80e49e74a7ccc98e1de71e8c0260f
Sedat Dayan <dayan.sedat1998@gmail.com>
2024-04-03 19:49:12 +0300

    initial commit

---
+++ app/src/main/java/com/example/fitnessapp/feature/welcome/splash/view/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fitnessapp.feature.welcome.splash.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fitnessapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
