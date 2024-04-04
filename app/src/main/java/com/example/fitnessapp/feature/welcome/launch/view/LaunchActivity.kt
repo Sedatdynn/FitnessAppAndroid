699ca82c5f411700697ab2fcd9cae24823ba57f3
Sedat Dayan <dayan.sedat1998@gmail.com>
2024-04-04 18:04:32 +0300

    login activity view created

--- app/src/main/java/com/example/fitnessapp/feature/welcome/launch/view/LaunchActivity.kt
+++ app/src/main/java/com/example/fitnessapp/feature/welcome/launch/view/LaunchActivity.kt
@@ -1,13 +1,23 @@
 package com.example.fitnessapp.feature.welcome.launch.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fitnessapp.R
+import com.example.fitnessapp.databinding.ActivityLaunchBinding
+import com.example.fitnessapp.feature.auth.LoginActivity
 
 class LaunchActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLaunchBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_launch)
+        binding = ActivityLaunchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.launchBtnEmail.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
