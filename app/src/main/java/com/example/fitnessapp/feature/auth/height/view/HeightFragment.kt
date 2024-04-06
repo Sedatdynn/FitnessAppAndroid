9ec89b07530b42b3c3133a78820d252ce08382fe
Sedat Dayan <dayan.sedat1998@gmail.com>
2024-04-06 17:44:40 +0300

    height fragment ui created

--- app/src/main/java/com/example/fitnessapp/feature/auth/height/view/HeightFragment.kt
+++ app/src/main/java/com/example/fitnessapp/feature/auth/height/view/HeightFragment.kt
@@ -1,15 +1,21 @@
 package com.example.fitnessapp.feature.auth.height.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
 import com.example.fitnessapp.R
+import com.example.fitnessapp.databinding.FragmentBirthDateBinding
+import com.example.fitnessapp.databinding.FragmentHeightBinding
+import java.util.Calendar
 
 
 class HeightFragment : Fragment() {
+    private lateinit var binding: FragmentHeightBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,8 +23,34 @@ class HeightFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_height, container, false)
+    ): View {
+        binding = FragmentHeightBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configureNumberPicker()
+        onNextClick()
+    }
+
+    private fun configureNumberPicker() {
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        binding.heightNumberPicker.apply {
+            minValue = 140
+            maxValue = 220
+            value = 170
+            setOnValueChangedListener { _, _, newVal ->
+                val selectedHeight = newVal
+                Log.d("SELECTED VALUE: ", selectedHeight.toString())
+            }
+        }
+    }
+
+    private fun onNextClick() {
+        binding.heightBtnNext.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_heightFragment_to_weightFragment)
+        }
     }
 }
